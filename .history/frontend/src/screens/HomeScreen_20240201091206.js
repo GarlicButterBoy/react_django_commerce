22ecfdcabb243913fb2b3cd9d963a85a0ec5e1fd
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2024-02-01 09:22:47 -0500

    implemented dispatch and useSelector

---
+++ .history/frontend/src/screens/HomeScreen_20240201091206.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import { useDispatch, useSelector } from "react-redux";
+import { } 
+
+function HomeScreen() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {}, []);
+
+  return (
+    <div>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+}
+
+export default HomeScreen;
--- .history/frontend/src/screens/HomeScreen_20240201092107.js
+++ .history/frontend/src/screens/HomeScreen_20240201092107.js
@@ -11,6 +11,7 @@ function HomeScreen() {
 
   useEffect(() => {
     dispatch(listProducts());
+
   }, [dispatch]);
 
   return (
