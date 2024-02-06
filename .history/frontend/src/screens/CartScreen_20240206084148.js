d447885ef15a8267b1a6043e4c00aa7f026a3818
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2024-02-06 09:15:48 -0500

    Created cart screen and added proper functionality

---
+++ .history/frontend/src/screens/CartScreen_20240206084148.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import { Message } from "../components/Message";
+import { addToCart } from "../actions/cartActions";
+
+function CartScreen({ match, location, history }) {
+  const productId = match.params.id;
+  const qty = location.search ? location.search.split("=")[1] : 1;
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+    return <Row>
+        <Col md={8}>
+            <h1></h1>
+        </Col>
+        <Col>
+        </Col>
+  </Row>;
+}
+
+export default CartScreen;
