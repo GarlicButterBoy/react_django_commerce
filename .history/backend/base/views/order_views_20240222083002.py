4bf9e2a48454d6e1c26b251205472268c90db33d
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2024-02-22 08:52:33 -0500

    Created Order View and URL functionality

---
+++ .history/backend/base/views/order_views_20240222083002.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from base.serializers import ProductSerializer
+from base.models import Product, Order, OrderItem, S
+
+from rest_framework import status
---
+++ .history/backend/base/views/order_views_20240222083843.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from base.serializers import ProductSerializer
+from base.models import Product, Order, OrderItem, ShippingAddress
+
+from rest_framework import status
+
+@api_view(['POST'])
+@permission_classes(['IsAuthenticated'])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+    
+    orderItems = data['orderItems']
+
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail': 'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        #(1) Create Order
+        order = Order.objects.create(
+            user=user,
+            paymentMethod=data['paymentMethod'],
+            taxPrice=data['taxPrice'],
+            shippingPrice=data['shippingPrice'],
+            totalPrice=data['totalPrice']
+        )
+
+        #(2) Create Shipping Address
+        shipping = ShippingAddress.objects.create(
+            data
+        )
+
+
+        #(3) Create Order Items and Set Order to orderItem Relationship
+
+        #(4) Update Stock
+
+    return Response('Order')
